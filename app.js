917296cb7e99882d919d8a9c7a951ee1ee3daaaa
Kristina Matuleviciute <kristina.matuleviciute@nearform.com>
2016-03-10 17:05:37 +0000

    new routes added

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
+var data = require('./routes/data');
+
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -14,6 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
+app.use('/data', data);
 
 
 // catch 404 and forward to error handler
