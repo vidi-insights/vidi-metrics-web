d94aa521fc95d6dbcff365244fbde9045cd4e5fa
Kristina Matuleviciute <kristina.matuleviciute@nearform.com>
2016-03-11 15:41:29 +0000

    data is sent to emitter

--- app.js
+++ app.js
@@ -5,18 +5,37 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var data = require('./routes/data');
-
+var emitter = require('vidi-metrics-emitter')
 var app = express();
-
+var emit = emitter();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(logger('dev'));
+// sapp.use(bodyParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/data', data);
+
+
+app.post("/data", function (req, res ){
+  var name = req.body.query
+  var foo = JSON.stringify({ title: name[0], value: name[1] });
+    // console.log(name[0], name[1]);
+    // console.log(foo);
+    // emit.emit(foo)
+    if (isNaN(name[1]) == true) {
+      console.log("wrong value, you need to enter number")
+      res.send(500, 'wrong value, enter just numbers please for your data field ') 
+    }
+    else {
+      emit.emit(foo)
+      console.log(foo);
+    }
+    
+      })
 
 
 // catch 404 and forward to error handler
--- routes/data.js
+++ routes/data.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 
-/* GET data page. */
+// GET data page. 
 router.get('/', function(req, res, next) {
   res.render('data', {
       title: 'vidi-metrics-web',
