7ccd8b208e7905bdd43c9151b05f9df02d90fab2
Kristina Matuleviciute <kristina.matuleviciute@nearform.com>
2016-05-12 10:57:56 +0100

    code comenting

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -11,20 +11,25 @@ import Settings from './Extra'
 export default class Layout extends React.Component {
   render () {
     const {location} = this.props
+    // giving style an obejct
     const containerStyle = {
       marginTop: '60px'
     }
     // header rendering DOM components
     return (
       <div> 
+      //loading header componant
       <Header/> 
+      //loading navigation bar
       <Nav location={location}/>       
       <div className='container' style={containerStyle}>
       <div className='row'>
-      <div className='col-lg-12'>    
+      <div className='col-lg-12'>   
+      // rendering children components  
       {this.props.children}
       </div>           
       </div> 
+      // loading footer component
       <Footer/>       
       </div>
       </div>    
